d1066e93043dd7e295687719d4e934fe2dcc9e8a
che <charles.v.he@gmail.com>
2015-03-05 16:32:04 +0800

    add UDP throughput testing code

--- src/main/java/charles/v/socket/Client.java
+++ src/main/java/charles/v/socket/Client.java
@@ -3,14 +3,18 @@ package charles.v.socket;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        new ThroughputClientThread().start();   // 50%
+//        new ThroughputClientThread().start();   // 50%
 //        new ThroughputClientThread().start();   // 75%
 //        new ThroughputClientThread().start();   // 100%
 //        new ThroughputClientThread().start();   // 100%
+        new ThroughputUdpClientThread().start();   // 100%
     }
 }
 
@@ -23,6 +27,22 @@ class ThroughputClientThread extends Thread {
             byte[] data = new byte[1024];
             while (true) {
                 bo.write(data, 0, data.length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThroughputUdpClientThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            DatagramSocket  socket = new DatagramSocket();
+            byte[] data = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("10.125.2.17"), 2048);
+            while (true) {
+                socket.send(packet);
             }
         } catch (IOException e) {
             e.printStackTrace();
